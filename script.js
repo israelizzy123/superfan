c9e576744d0baa4332c71186e55825282f2ea265
israelizzy123 <31420706+israelizzy123@users.noreply.github.com>
2017-12-12 15:48:10 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 
-function show(elem){
-	elem.classList.toggle("madison");
-	elem.classList.toggle("jude")
+
+function toggleBlueBackground(elem){
+	document.getElementById("evan").classList.remove("blue");
 }
 
+function toggleBlueBackground2(el){
+	Document.getElementById("jude").classList.remove("blue");
+}
+
+function toggleBlueBackground3(e){
+	document.getElementById("madison").classList.remove("blue")
+}
+
+function toggleBlueBackground4(ele){
+	document.getElementById("finn").classList.remove("blue")
+}
